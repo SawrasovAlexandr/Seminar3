f5e067d05ce664a62cf239b7fd2860aee1505cf3
AlexandrSawrasov <caiiiuk@mail.ru>
2022-10-22 23:15:17 +0300

    add Task19 3

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -15,18 +15,52 @@ int InputInt(string? value)
     return number;
 }
 
-Console.Write("Введите пятизначное число: ");
-int num = InputInt(Console.ReadLine());
-if (num > 9999 && num < 100000)
-{
-    int digit1 = num / 10000,
-        digit2 = num / 1000 % 10,
-        digit4 = num / 10 % 10,
-        digit5 = num % 10;
-    Console.Write(digit1 == digit5 && digit2 == digit4 ? "Является полиндромом" : "Не является полиндромом");
-}
-else Console.Write("Введенное выражение не является пятизначным числом");
+// Console.Write("Введите пятизначное число: ");
+// int num = InputInt(Console.ReadLine());
+// if (num > 9999 && num < 100000)
+// {
+//     int digit1 = num / 10000,
+//         digit2 = num / 1000 % 10,
+//         digit4 = num / 10 % 10,
+//         digit5 = num % 10;
+//     Console.Write(digit1 == digit5 && digit2 == digit4 ? "Является полиндромом" : "Не является полиндромом");
+// }
+// else Console.Write("Введенное выражение не является пятизначным числом");
 
+
+//Принимает на вход любое число и проверяет, является ли оно палиндромом.
+
+int DigitCount(int number)
+{
+    int count = 1;
+    int num = number;
+    while (num > 10)
+    {
+        num /= 10;
+        count++;
+    }
+    return count;
+}
+
+bool PalindroCheck(int number)
+{
+    int i = 1;
+    int j = Convert.ToInt32(Math.Pow(10, DigitCount(number) - 1));
+    bool palindrom = true;
+    while (palindrom && i < j)
+    {
+        int digitA = number / j % 10;
+        int digitZ = number / i % 10;
+        palindrom = digitA == digitZ;
+        i *= 10;
+        j /= 10;
+    }
+    return palindrom;
+}
+
+ Console.Write("Введите число: ");
+int num = InputInt(Console.ReadLine());
+Console.Write(PalindroCheck(num) ? "Является полиндромом" : "Не является полиндромом");
 
 
 
